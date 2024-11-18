6e9bdf1f5c1be8dd7f66d532501d1618cbdc8c8f
Takumi Yamada <110591229+takumi-ya@users.noreply.github.com>
2024-11-18 21:42:32 +0900

    リクエストハンドラを実装

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -27,6 +28,25 @@ type Todo struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time `bun:",nullzero"`
 	DeletedAt time.Time `bun:",soft_delete,nullzero"`
+}
+
+type Data struct {
+	Todos  []Todo
+	Errors []error
+}
+
+func customFunc(todo *Todo) func([]string) []error {
+	return func(values []string) []error {
+		if len(values) == 0 {
+			return nil
+		}
+		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
+		if err != nil {
+			return []error{echo.NewBindingError("until", values[0:1], "failed to decode time", err)}
+		}
+		todo.Until = dt
+		return nil
+	}
 }
 
 func main() {
@@ -57,7 +77,68 @@ func main() {
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "")
+		var todos []Todo
+		ctx := context.Background()
+		err := db.NewSelect().Model(&todos).Scan(ctx)
+		if err != nil {
+			e.Logger.Error(err)
+			return c.Render(http.StatusBadRequest, "index", Data{
+				Errors: []error{errors.New("Cannot get todos")},
+			})
+		}
+		return c.Render(http.StatusOK, "index", Data{Todos: todos})
 	})
+
+	e.POST("/", func(c echo.Context) error {
+		var todo Todo
+		// フォームパラメータをフィールドにバインド
+		errs := echo.FormFieldBinder(c).
+			Int64("id", &todo.ID).
+			String("content", &todo.Content).
+			Bool("done", &todo.Done).
+			CustomFunc("until", customFunc(&todo)).
+			BindErrors()
+
+		if errs != nil {
+			e.Logger.Error(errs)
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: errs})
+		} else if todo.ID == 0 {
+			// IDが0のときは登録
+			ctx := context.Background()
+			if todo.Content == "" {
+				err = errors.New("Todo not found")
+			} else {
+				_, err = db.NewInsert().Model(&todo).Exec(ctx)
+				if err != nil {
+					e.Logger.Error(err)
+					err = errors.New("Cannot update")
+				}
+			}
+		} else {
+			ctx := context.Background()
+			if c.FormValue("delete") != "" {
+				// 削除
+				_, err = db.NewDelete().Model(&todo).Where("id = ?", todo.ID).Exec(ctx)
+			} else {
+				// 更新
+				var orig Todo
+				err = db.NewSelect().Model(&orig).Where("id = ?", todo.ID).Scan(ctx)
+				if err == nil {
+					orig.Done = todo.Done
+					_, err = db.NewUpdate().Model(&orig).Where("id = ?", todo.ID).Exec(ctx)
+				}
+			}
+			if err != nil {
+				e.Logger.Error(err)
+				err = errors.New("Cannot update")
+			}
+		}
+		if err != nil {
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: []error{err}})
+		}
+		return c.Redirect(http.StatusFound, "/")
+
+	})
+
 	e.Logger.Fatal(e.Start(":8989"))
 }
